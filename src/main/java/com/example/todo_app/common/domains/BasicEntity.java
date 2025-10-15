5ddeff62c404f796683427ecdca8e1e9e6800229
josephus <josephusmupanda48@gmail.com>
2025-10-15 11:19:17 +0200

    add BasicEntity

---
+++ src/main/java/com/example/todo_app/common/domains/BasicEntity.java
@@ -0,0 +1,32 @@
+package com.example.todo_app.common.domains;
+import com.example.todo_app.common.annotations.UuidV7;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BasicEntity {
+    @Id
+    @UuidV7()
+    private String id;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
+
