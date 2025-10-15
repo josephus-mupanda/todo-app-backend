6ea148b294eab2bebf0c7612ed323b95ba3203b2
josephus <josephusmupanda48@gmail.com>
2025-10-15 11:22:39 +0200

    add Role

---
+++ src/main/java/com/example/todo_app/common/domains/Role.java
@@ -0,0 +1,33 @@
+package com.example.todo_app.common.domains;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+@SQLDelete(sql = "UPDATE roles SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class Role extends BasicEntity {
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean managed = true;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "role_permissions", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<Permission> permissions = new HashSet<>();
+}
